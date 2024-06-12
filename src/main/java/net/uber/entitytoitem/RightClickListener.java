9a81d6c5764cea320649fd79776a65467c9acaa2
James Homer <jameswatsonhomer@gmail.com>
2024-06-12 17:30:49 +0200

    implement entitytype check

--- src/main/java/net/uber/entitytoitem/RightClickListener.java
+++ src/main/java/net/uber/entitytoitem/RightClickListener.java
@@ -1,17 +1,36 @@
 package net.uber.entitytoitem;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class RightClickListener implements Listener {
+
+    private final @NotNull List<String> allowedEntityList;
+
+    public RightClickListener(@NotNull List<String> allowedEntityList) {
+
+        this.allowedEntityList = allowedEntityList;
+
+    }
+
+    public boolean isAllowedEntity(Entity entity) {
+
+        return allowedEntityList.contains(entity.getType().toString());
+
+    }
 
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
-        if (event.getRightClicked() != null) {
-            player.sendMessage("You right-clicked an entity!");
+        Entity entity = event.getRightClicked();
+        if (isAllowedEntity(entity)) {
+            player.sendMessage("You right-clicked an allowed entity!");
         }
     }
 
