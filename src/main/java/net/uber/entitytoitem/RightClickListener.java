526d563f4b7783043422963efa43d56d653c81f6
James Homer <jameswatsonhomer@gmail.com>
2024-06-24 10:46:53 +0200

    used AI to clean things up

--- src/main/java/net/uber/entitytoitem/RightClickListener.java
+++ src/main/java/net/uber/entitytoitem/RightClickListener.java
@@ -17,171 +17,139 @@ import java.util.*;
 
 public class RightClickListener implements Listener {
 
-    private final List<String> allowedEntityList;
-    private final List<String> allowedItemStringList;
-    private final boolean drop;
-    private final boolean consumable;
-    private final boolean tamedProtection;
-    private final boolean loseInventory;
-
+    private final List<String> allowedEntityTypes;
+    private final List<String> allowedItemStrings;
+    private final boolean shouldDrop;
+    private final boolean isConsumable;
+    private final boolean hasTamedProtection;
+    private final boolean shouldLoseInventory;
     private final String alreadyTamedMessage;
 
-    ConfigManager configManager;
-
     public RightClickListener(ConfigManager configManager) {
-
-        this.configManager = configManager;
-
         FileConfiguration config = configManager.getConfig();
-
-        this.allowedEntityList = config.getStringList("allowedEntityList");
-        this.allowedItemStringList = config.getStringList("item");
-        this.drop = config.getBoolean("drop");
-        this.consumable = config.getBoolean("consumable");
-        this.tamedProtection = config.getBoolean("tamed-protection");
-        this.loseInventory = config.getBoolean("lose-inventory");
-
+        this.allowedEntityTypes = config.getStringList("allowedEntityList");
+        this.allowedItemStrings = config.getStringList("item");
+        this.shouldDrop = config.getBoolean("drop");
+        this.isConsumable = config.getBoolean("consumable");
+        this.hasTamedProtection = config.getBoolean("tamed-protection");
+        this.shouldLoseInventory = config.getBoolean("lose-inventory");
         this.alreadyTamedMessage = config.getString("messages.already-tamed");
-
     }
 
     public boolean isAllowedEntity(Entity entity) {
-
-        return allowedEntityList.contains(entity.getType().toString());
-
+        return allowedEntityTypes.contains(entity.getType().toString());
     }
 
     public boolean isPlayerHoldingAllowedItem(Player player) {
-        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
-        return isAllowedItem(itemInMainHand);
+        return isAllowedItem(player.getInventory().getItemInMainHand());
     }
 
     public boolean isAllowedItem(ItemStack item) {
-        for (String itemString : this.allowedItemStringList) {
-            if (isSameItem(item, itemString)) {
-                return true;
-            }
-        }
-        return false;
+        return allowedItemStrings.stream().anyMatch(itemString -> isSameItem(item, itemString));
     }
 
-    public boolean isSameItem(ItemStack item, String allowedItemString) {
-
+    private boolean isSameItem(ItemStack item, String allowedItemString) {
         if (ItemInfoExtractor.isOraxen(allowedItemString)) {
-            /*
-            if (!OraxenItems.exists(item)) {
-                return false;
-            }
-            String allowedID = OraxenItems.getIdByItem(item);
-            String itemID = ItemInfoExtractor.extractOraxen(this.allowedItemString);
-            return Objects.equals(allowedID, itemID);
-             */
+            // Oraxen item check logic here
             return false;
         }
 
         if (ItemInfoExtractor.isCMD(allowedItemString)) {
             int cmd = ItemInfoExtractor.extractCMD(allowedItemString);
-            ItemMeta meta = item.getItemMeta();
-            if (meta != null && meta.hasCustomModelData()) {
-                int itemCMD = meta.getCustomModelData();
-                return cmd == itemCMD;
-            } else {
-                return false;
-            }
+            return item.getItemMeta() != null &&
+                    item.getItemMeta().hasCustomModelData() &&
+                    item.getItemMeta().getCustomModelData() == cmd;
         }
 
         return Objects.equals(item.getType().toString(), allowedItemString);
-
     }
 
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
         Entity entity = event.getRightClicked();
-        if (isAllowedEntity(entity) && isPlayerHoldingAllowedItem(player)) {
 
-            List<ItemStack> itemsToDrop = new ArrayList<>();
+        if (!isAllowedEntity(entity) || !isPlayerHoldingAllowedItem(player)) {
+            return;
+        }
 
-            if (tamedProtection && entity instanceof Tameable tameable) {
-                if (tameable.getOwner() != null && player.getUniqueId() != tameable.getOwner().getUniqueId()) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', alreadyTamedMessage));
-                    return;
+        if (hasTamedProtection && !canInteractWithTamedEntity(player, entity)) {
+            return;
+        }
+
+        List<ItemStack> itemsToDrop = collectItemsToDrop(entity);
+        ItemStack entityEgg = EntityConverter.getEntityEgg(entity);
+        itemsToDrop.add(entityEgg);
+
+        if (isConsumable) {
+            removeItemFromHand(player);
+        }
+
+        entity.remove();
+
+        handleItemDistribution(player, entity.getLocation(), itemsToDrop);
+
+        player.playSound(player, Sound.ENTITY_ENDERMAN_TELEPORT, SoundCategory.MASTER, 0.1f, 2f);
+    }
+
+    private boolean canInteractWithTamedEntity(Player player, Entity entity) {
+        if (entity instanceof Tameable tameable && tameable.getOwner() != null) {
+            if (!player.getUniqueId().equals(tameable.getOwner().getUniqueId())) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', alreadyTamedMessage));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<ItemStack> collectItemsToDrop(Entity entity) {
+        List<ItemStack> itemsToDrop = new ArrayList<>();
+        if (shouldLoseInventory && entity instanceof InventoryHolder inventoryHolder) {
+            for (ItemStack item : inventoryHolder.getInventory().getContents()) {
+                if (item != null) {
+                    itemsToDrop.add(item);
                 }
             }
+            inventoryHolder.getInventory().clear();
+        }
+        return itemsToDrop;
+    }
 
-            Location location = entity.getLocation();
+    private void handleItemDistribution(Player player, Location location, List<ItemStack> itemsToDrop) {
+        if (shouldDrop) {
+            dropItems(location, itemsToDrop);
+        } else {
+            addItemsToPlayerInventory(player, location, itemsToDrop);
+        }
+    }
 
-            if (loseInventory && entity instanceof InventoryHolder inventoryHolder) {
-                Inventory inventory = inventoryHolder.getInventory();
-                ItemStack[] itemList = inventory.getContents();
-                if (itemList.length > 0) {
-                    for (ItemStack itemInQuestion : itemList) {
-                        if (itemInQuestion != null) {
-                            itemsToDrop.add(itemInQuestion);
-                        }
-                    }
-                    inventory.clear();
+    private void dropItems(Location location, List<ItemStack> items) {
+        for (ItemStack item : items) {
+            if (item != null) {
+                Objects.requireNonNull(location.getWorld()).dropItemNaturally(location, item);
+            }
+        }
+    }
+
+    private void addItemsToPlayerInventory(Player player, Location location, List<ItemStack> items) {
+        for (ItemStack item : items) {
+            if (item != null) {
+                if (player.getInventory().firstEmpty() != -1) {
+                    player.getInventory().addItem(item);
+                } else {
+                    Objects.requireNonNull(location.getWorld()).dropItemNaturally(location, item);
                 }
             }
-
-            ItemStack entityEgg = EntityConverter.getEntityEgg(entity);
-
-            itemsToDrop.add(entityEgg);
-
-            if (consumable) {
-                removeItemFromHand(player);
-            }
-
-            entity.remove();
-
-            if (drop) {
-
-                for (ItemStack toDrop : itemsToDrop) {
-                    if (toDrop != null) {
-                        Objects.requireNonNull(location.getWorld()).dropItemNaturally(location, toDrop);
-                    }
-                }
-
-            } else {
-                /*
-                HashMap<Integer, ItemStack> leftoverItems = player.getInventory().addItem(entityEgg);
-                if (!leftoverItems.isEmpty()) {
-                    //Should be only 1 idgaf
-                    int amount = leftoverItems.keySet().
-                    Collection<ItemStack> itemstack = leftoverItems.values();
-                    location.getWorld().dropItemNaturally(location, entityEgg);
-                }
-                 */
-
-                //additem return value is a better method but I don't have internet rn and Idk how to deal with the hashmaps
-
-                for (ItemStack toDrop : itemsToDrop) {
-                    if (toDrop != null) {
-                        //because idk how to do the additem method, it just repeatedly checks for first item
-                        if (player.getInventory().firstEmpty() != -1) {
-                            player.getInventory().addItem(toDrop);
-                        } else {
-                            Objects.requireNonNull(location.getWorld()).dropItemNaturally(location, toDrop);
-                        }
-                    }
-                }
-
-            }
-
-            player.playSound(player, Sound.ENTITY_ENDERMAN_TELEPORT, SoundCategory.MASTER, 0.1f, 2f);
-
         }
     }
 
     private void removeItemFromHand(Player player) {
         PlayerInventory inventory = player.getInventory();
         ItemStack mainHand = inventory.getItemInMainHand();
-        int currentAmount = mainHand.getAmount();
-        if (currentAmount == 1) {
+        if (mainHand.getAmount() == 1) {
             inventory.setItemInMainHand(null);
         } else {
-            mainHand.setAmount(currentAmount - 1);
+            mainHand.setAmount(mainHand.getAmount() - 1);
         }
     }
-
 }
